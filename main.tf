ff4cfebcb711fdd1b1d7582c559d1f0bc4396e16
Akintunde <Akin.cloudsec@gmail.com>
2026-05-15 01:33:33 -0400

    Day 7/30: Add Checkov skips + use AWS KMS encryption

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+#checkov:skip=CKV2_AWS_62:Event notifications not required for Day 7 lab
+#checkov:skip=CKV2_AWS_61:Lifecycle policies not required for Day 7 lab
+#checkov:skip=CKV_AWS_18:Logging requires separate bucket - Day 9 topic
+#checkov:skip=CKV_AWS_144:CRR not required for lab environment
 # Day 7/30: CIS-Hardened S3 Bucket for Cloud Security Lab
 
 resource "aws_s3_bucket" "security_lab_bucket" {
@@ -28,20 +32,13 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
+#checkov:skip=CKV_AWS_145:Using AWS managed key for Day 7, custom KMS is Day 10
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   bucket = aws_s3_bucket.security_lab_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm = "aws:kms" # KMS instead of AES256 to satisfy Checkov
     }
   }
 }
-
-# Logging commented out for Day 7 - requires separate bucket
-# resource "aws_s3_bucket_logging" "logging" {
-#   bucket = aws_s3_bucket.security_lab_bucket.id
-# 
-#   target_bucket = aws_s3_bucket.security_lab_bucket.id
-#   target_prefix = "access-logs/"
-# }
