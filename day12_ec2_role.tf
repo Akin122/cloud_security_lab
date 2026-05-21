eb6fd09101b5aa28ccd080a2272c5d7121d6e33d
Akin122 <Akin.cloudsec@gmail.com>
2026-05-21 04:27:11 -0400

    Fix terraform format

--- day12_ec2_role.tf
+++ day12_ec2_role.tf
@@ -24,19 +24,19 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 }
 
 resource "aws_instance" "demo_ec2" {
-  ami                   = "ami-0c02fb559556c7d316" # Amazon Linux 2023 in us-east-1. Change if needed
-  instance_type         = "t2.micro"
-  iam_instance_profile  = aws_iam_instance_profile.ec2_profile.name
-  monitoring            = true
+  ami                  = "ami-0c02fb559556c7d316" # Amazon Linux 2023 in us-east-1. Change if needed
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  monitoring           = true
 
   metadata_options {
-    http_tokens   = "required"  # Disables IMDSv1, forces IMDSv2
+    http_tokens   = "required" # Disables IMDSv1, forces IMDSv2
     http_endpoint = "enabled"
   }
 
   root_block_device {
     encrypted  = true
-    kms_key_id = null  # Uses default AWS EBS KMS key
+    kms_key_id = null # Uses default AWS EBS KMS key
   }
 
   ebs_optimized = true
